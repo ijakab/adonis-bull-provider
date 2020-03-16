168f5f1e4fd957db1eecf7d241b23cd2b7d42b12
Ivan Jakab <ivan.jakab@gauss.hr>
2020-03-16 10:34:44 +0100

    feat: handling error on events

--- src/BaseQueue.js
+++ src/BaseQueue.js
@@ -12,9 +12,20 @@ class BaseQueue {
         let queue = this.getQueueInstance()
         let instance = new this()
     
-        queue.process('*', (payload) => {
-            if(payload.name && instance[payload.name]) return instance[payload.name](payload)
-            if(instance.defaultHandle) return instance.defaultHandle(payload)
+        queue.process('*', async (payload) => {
+            try {
+                if(payload.name && instance[payload.name]) return await instance[payload.name](payload)
+                if(instance.defaultHandle) return await instance.defaultHandle(payload)
+            } catch (e) {
+                try {
+                    if(instance.onError) return await instance.onError(e, payload)
+                } catch (err) {
+                    console.error('Unhandled error on handling bool error')
+                    console.error('%o', err)
+                }
+                console.error('Unhandled error on bool event')
+                console.error('%o', e)
+            }
         })
         
         return queue
